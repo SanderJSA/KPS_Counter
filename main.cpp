01b5c7e18b6574b0b38a0b783431a21e82fbd9e6
SanderJSA <sanderjsa@hotmail.com>
2019-04-01 19:48:15 -0400

    fltk is unusable

--- main.cpp
+++ main.cpp
@@ -2,40 +2,62 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Value_Output.H>
 #include <FL/Fl_Button.H>
+#include <memory>
+
+#ifdef _WIN32
+#include <Winuser.h>
+#else
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <error.h>
+#endif
 
 #define TIMER_DELAY 0.005
 
 long totalPresses = 0;
 
-void setupGUI(int argc, char **argv);
-void setupCB();
+Fl_Value_Output *setupGUI(int argc, char **argv);
+void setupCB(Fl_Value_Output *window);
 void windowsCB(void *data);
 void linuxCB(void *data);
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
 	//Setup GUI and variables
-	setupGUI(argc, argv);
+	auto *window = setupGUI(argc, argv);
 
 	//Creating timer callback
-	setupCB();
+	setupCB(window);
 
+	//Run program
 	return Fl::run();
 }
 
-void setupGUI(int argc, char **argv)
+
+Fl_Value_Output *setupGUI(int argc, char **argv)
 {
 	Fl_Window *window = new Fl_Window(500,500);
 	Fl_Value_Output *totPressesCounter = new Fl_Value_Output(10, 20, 100, 40);
 	totPressesCounter->textsize(20);
 	totPressesCounter->label("                   Total number of keypresses");
 	totPressesCounter->align(FL_ALIGN_TOP);
+	totPressesCounter->value(totalPresses);
 
 	window->end();
 	window->show(argc, argv);
+
+	return totPressesCounter;
 }
 
-void setupCB()
+struct Data
+{
+	int input_fd;
+	Fl_Value_Output *windows;
+};
+
+void setupCB(Fl_Value_Output *window)
 {
 #ifdef _WIN32
 	Fl::add_timeout(TIMER_DELAY, windowsCB);
@@ -44,10 +66,11 @@ void setupCB()
 	if (input_fd == -1) {
 			error(EXIT_FAILURE, errno, "Error opening input event device");
 	}
-	Fl::add_timeout(TIMER_DELAY, linuxCB, &input_fd);
+	Fl::add_timeout(TIMER_DELAY, linuxCB, new Data{input_fd, window});
 #endif
 }
 
+#ifdef _WIN32
 void windowsCB(void *data)
 {
 	if (GetAsyncKeyState(VK_SPACE)!= 0)
@@ -60,24 +83,25 @@ void windowsCB(void *data)
 	Fl::repeat_timeout(TIMER_DELAY, windowsCB, data);
 }
 
+#else
+
 struct input_event {
 	struct timeval time;
 	unsigned short type;
 	unsigned short code;
 	unsigned int value;
 };
-/*
 
 void linuxCB(void *data)
 {
-	int input_fd = *(int*)data;
 	input_event event{};
-		while (read(input_fd, &event, sizeof(input_event)) > 0) {
-			std::cout << "input_event { .time = { .tv_sec = " << event.time.tv_sec
-			          << ", .tv_usec = " << event.time.tv_usec
-			          << " }, .type = " << event.type
-			          << ", .code = " << event.code
-			          << ", .value = " << event.value << " }" << std::endl;
-		}
+	while (read(((Data*)data)->input_fd, &event, sizeof(input_event)) > 0)
+	{
+		totalPresses++;
+		((Data*)data)->windows->value(totalPresses);
+		((Data*)data)->windows->redraw();
 	}
-}*/
+
+	Fl::repeat_timeout(TIMER_DELAY, linuxCB, data);
+}
+#endif
